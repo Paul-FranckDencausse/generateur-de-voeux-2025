b0c96782c8c1bc4c742eb86754a7ec200bc10ff3
Paul-Franck Dencausse <121952248+Paul-FranckDencausse@users.noreply.github.com>
2025-01-17 09:54:26 +0100

    tutoiement

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Liste de messages de Bonne Année
 const messages = [
     "Bonne Année 2025 ! Que cette année soit remplie de bonheur et de succès.",
-    "Meilleurs vœux pour 2025 ! Que tous vos rêves se réalisent.",
-    "Que 2025 vous apporte joie, santé et prospérité. Bonne Année !",
-    "Bonne Année 2025 ! Que cette nouvelle année soit merveilleuse pour vous et vos proches.",
+    "Meilleurs vœux pour 2025 ! Que tous tes rêves se réalisent.",
+    "Que 2025 t'apporte joie, santé et prospérité. Bonne Année !",
+    "Bonne Année 2025 ! Que cette nouvelle année soit merveilleuse pour toi et tes proches.",
     "Félicitations pour une nouvelle année pleine de nouvelles opportunités et de succès. Bonne Année 2025 !"
 ];
 
